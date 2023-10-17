90fdaf32c27a95d6d183740eda42d7602cb1cc5f
SajeedAninda <sajaninda@gmail.com>
2023-10-17 21:42:00 +0600

    Created The brand intro section

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import BrandIntro from '../Brand Intro/BrandIntro';
 
 const Homepage = () => {
     return (
         <div>
             <Banner></Banner>
+            <BrandIntro></BrandIntro>
         </div>
     );
 };
